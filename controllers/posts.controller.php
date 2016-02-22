4bc4929c6376a4ed79c35b56240ccd714fb6dab4
taras <tgamanov@gmail.com>
2016-02-22 02:09:13 +0200

    img file checking added

--- controllers/posts.controller.php
+++ controllers/posts.controller.php
@@ -13,12 +13,17 @@ class PostsController extends Controller
         if (isset($_POST['submit']))//check if submit button has been pressed
         {
             $this->data['post'] = $this->model->getvisiblelist();
-            $name = $_POST['name'];
-            $email = $_POST['email'];
-            $message = $_POST['message'];
-            $photo_path4DB = '';
-            $image = $_FILES['image'];
-            $marker = true;
+            $name = isset($_POST['name'])?$_POST['name']:null;
+//            $name = $_POST['name'];
+            $email = isset($_POST['email'])?$_POST['email']:null;
+//            $email = $_POST['email'];
+            $message = isset( $_POST['message'])? $_POST['message']:null ;
+//            $message = $_POST['message'];
+            $photo_path4DB = isset($photo_path4DB)?'':null;
+//            $photo_path4DB = '';
+            $image = isset( $_FILES['image'])? $_FILES['image']:null ;
+//            $image = $_FILES['image'];
+
             if (isset($image))//check if file has been uploaded
             {
 
@@ -36,19 +41,24 @@ class PostsController extends Controller
 
                     return $key;
                 }
-
-                $file_ext = explode('.', $file_name);
+//                $query_age = (isset($_GET['query_age']) ? $_GET['query_age'] : null);
+                $file_ext = isset($file_name)?explode('.',$file_name):null;
+//                $file_ext = explode('.', $file_name);
                 $file_ext = strtolower(end($file_ext));
-                $file_tmp = $image['tmp_name'];
-                $file_name = random_string(7) . '.' . $file_ext;
-                $photo_path4DB = DS . $location . DS . $file_name;
+                $file_tmp = isset($image['tmp_name'])?$image['tmp_name']:null;
+//                $file_tmp = $image['tmp_name'];
+                $file_name = isset($file_ext)?random_string(7) . '.' . $file_ext:null;
+//                $file_name = random_string(7) . '.' . $file_ext;
+                $photo_path4DB = isset($file_name)?DS . $location . DS . $file_name:null;
+//                $photo_path4DB = DS . $location . DS . $file_name;
+
                 $allowed_ext = array("jpg", "jpeg", "gif", "png");
 
                 if (in_array($file_ext, $allowed_ext) === false) {//check if extension of the file is correct
 
                     echo '<div class="notice-box bg-danger padding-30">Extension of the photo is not allowed, please choose next type of the file:  JPEG, GIF or PNG.</div>';
                     echo "<meta http-equiv=\"refresh\" content=\"15;url=/\" />";
-                    return $marker = false;
+                    return false;
                 } else {
                     move_uploaded_file($file_tmp, $location . '/' . $file_name);//upload photo to disk
                     $convert = new SimpleImage();//resize photo to 320*240
@@ -59,13 +69,10 @@ class PostsController extends Controller
 
 
             }
-
-            if ($marker) {
                 $this->model->write2db($name, $email, $message, $photo_path4DB);
                 Session::setFlash('Message has been sent for validation.');
                 //Router::redirect('/');
                 echo "<meta http-equiv=\"refresh\" content=\"3;url=/\" />";
-            }
         }
         $this->data = $this->model->getvisiblelist();
     }
